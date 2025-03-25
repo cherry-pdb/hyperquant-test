02bac5355d4e59c990a2c474d4d974c94844f4bb
cherry-pdb <mark.v.nazarov@yandex.ru>
2025-03-25 09:03:24 +0300

    added helpers for rest client

---
+++ TestHQ.Core/Helpers/PeriodConverter.cs
@@ -0,0 +1,40 @@
+﻿namespace TestHQ.Core.Helpers;
+
+public static class PeriodConverter
+{
+    public static string GetPeriodString(int periodInSec)
+    {
+        if (periodInSec <= 0)
+            throw new ArgumentOutOfRangeException(nameof(periodInSec), "Period in seconds must be a positive.");
+
+        const int oneMinute = 60;
+        const int fiveMinutes = 5 * oneMinute;
+        const int fifteenMinutes = 15 * oneMinute;
+        const int thirtyMinutes = 30 * oneMinute;
+        const int oneHour = 60 * oneMinute;
+        const int threeHours = 3 * oneHour;
+        const int sixHours = 6 * oneHour;
+        const int twelveHours = 12 * oneHour;
+        const int oneDay = 24 * oneHour;
+        const int oneWeek = 7 * oneDay;
+        const int fourteenDays = 14 * oneDay;
+        const int oneMonth = 30 * oneDay;
+
+        return periodInSec switch
+        {
+            oneMinute => "1m",
+            fiveMinutes => "5m",
+            fifteenMinutes => "15m",
+            thirtyMinutes => "30m",
+            oneHour => "1h",
+            threeHours => "3h",
+            sixHours => "6h",
+            twelveHours => "12h",
+            oneDay => "1D",
+            oneWeek => "1W",
+            fourteenDays => "14D",
+            oneMonth => "1M",
+            _ => throw new ArgumentException("Invalid period in seconds for API. Check available values here: https://docs.bitfinex.com/reference/rest-public-candles")
+        };
+    }
+}
---
+++ TestHQ.Core/Helpers/UrlBuilder.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+
+namespace TestHQ.Core.Helpers;
+
+public class UrlBuilder
+{
+    private const char UrlDelimiter = '/';
+    
+    private readonly StringBuilder _url;
+    private readonly List<string> _paths;
+    private readonly Dictionary<string, string> _queryParams;
+
+    public UrlBuilder(string baseUrl)
+    {
+        _url = new StringBuilder(baseUrl.TrimEnd(UrlDelimiter));
+        _paths = new List<string>();
+        _queryParams = new Dictionary<string, string>();
+    }
+    
+    public UrlBuilder AddPath(string path)
+    {
+        if (string.IsNullOrWhiteSpace(path))
+            throw new ArgumentException("Path can't be null or empty.", nameof(path));
+
+        _paths.Add(path.Trim(UrlDelimiter));
+        
+        return this;
+    }
+    
+    public UrlBuilder AddParameter(string paramName, string? paramValue)
+    {
+        if (string.IsNullOrWhiteSpace(paramName))
+            throw new ArgumentException("Parameter name can't be null or empty.", nameof(paramName));
+
+        if (!string.IsNullOrWhiteSpace(paramValue))
+            _queryParams[paramName] = paramValue;
+
+        return this;
+    }
+    
+    public string Build()
+    {
+        if (_paths.Count != 0)
+        {
+            _url.Append(UrlDelimiter).Append(string.Join(UrlDelimiter, _paths));
+        }
+
+        if (_queryParams.Count != 0)
+        {
+            var queryString = string.Join("&",
+                _queryParams.Select(kvp => $"{Uri.EscapeDataString(kvp.Key)}={Uri.EscapeDataString(kvp.Value)}"));
+            _url.Append('?')
+                .Append(queryString);
+        }
+
+        return _url.ToString();
+    }
+}
